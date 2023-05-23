8b30435c10ddbfc1bf380201b29a023361c15911
Neslihansolhan <117343701+Neslihansolhan@users.noreply.github.com>
2023-05-23 11:01:14 +0300

    Add files via upload

---
+++ chart/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.chcMarkers = new System.Windows.Forms.CheckBox();
+            this.chcFast = new System.Windows.Forms.CheckBox();
+            this.chcColumn = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackRotation = new System.Windows.Forms.TrackBar();
+            this.trackDepth = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackWidth = new System.Windows.Forms.TrackBar();
+            this.chc3D = new System.Windows.Forms.CheckBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timerChart = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDepth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 546;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(546, 450);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.chcMarkers, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.chcFast, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.chcColumn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.chc3D, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnStart, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnStop, 0, 7);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.11953F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 37.31778F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(250, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // chcMarkers
+            // 
+            this.chcMarkers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.chcMarkers.AutoSize = true;
+            this.chcMarkers.Location = new System.Drawing.Point(93, 105);
+            this.chcMarkers.Name = "chcMarkers";
+            this.chcMarkers.Size = new System.Drawing.Size(64, 38);
+            this.chcMarkers.TabIndex = 5;
+            this.chcMarkers.Text = "Markers";
+            this.chcMarkers.UseVisualStyleBackColor = true;
+            this.chcMarkers.CheckedChanged += new System.EventHandler(this.chcMarkers_CheckedChanged);
+            // 
+            // chcFast
+            // 
+            this.chcFast.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.chcFast.AutoSize = true;
+            this.chcFast.Location = new System.Drawing.Point(102, 71);
+            this.chcFast.Name = "chcFast";
+            this.chcFast.Size = new System.Drawing.Size(46, 28);
+            this.chcFast.TabIndex = 4;
+            this.chcFast.Text = "Fast";
+            this.chcFast.UseVisualStyleBackColor = true;
+            this.chcFast.CheckedChanged += new System.EventHandler(this.chcFast_CheckedChanged);
+            // 
+            // chcColumn
+            // 
+            this.chcColumn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.chcColumn.AutoSize = true;
+            this.chcColumn.Location = new System.Drawing.Point(94, 3);
+            this.chcColumn.Name = "chcColumn";
+            this.chcColumn.Size = new System.Drawing.Size(61, 28);
+            this.chcColumn.TabIndex = 3;
+            this.chcColumn.Text = "Column";
+            this.chcColumn.UseVisualStyleBackColor = true;
+            this.chcColumn.CheckedChanged += new System.EventHandler(this.chcColumn_CheckedChanged);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.22222F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.77778F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.trackRotation, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.trackDepth, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.trackWidth, 2, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 149);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.88172F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.11828F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(244, 121);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(126, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Border Width";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(55, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Point Depth";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackRotation
+            // 
+            this.trackRotation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.trackRotation.Location = new System.Drawing.Point(3, 35);
+            this.trackRotation.Name = "trackRotation";
+            this.trackRotation.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackRotation.RightToLeftLayout = true;
+            this.trackRotation.Size = new System.Drawing.Size(45, 83);
+            this.trackRotation.TabIndex = 0;
+            this.trackRotation.TickFrequency = 2;
+            this.trackRotation.Scroll += new System.EventHandler(this.trackRotation_Scroll);
+            // 
+            // trackDepth
+            // 
+            this.trackDepth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.trackDepth.Location = new System.Drawing.Point(65, 35);
+            this.trackDepth.Name = "trackDepth";
+            this.trackDepth.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackDepth.RightToLeftLayout = true;
+            this.trackDepth.Size = new System.Drawing.Size(45, 83);
+            this.trackDepth.TabIndex = 1;
+            this.trackDepth.TickFrequency = 2;
+            this.trackDepth.Scroll += new System.EventHandler(this.trackDepth_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rotation";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackWidth
+            // 
+            this.trackWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.trackWidth.Location = new System.Drawing.Point(140, 35);
+            this.trackWidth.Name = "trackWidth";
+            this.trackWidth.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackWidth.RightToLeftLayout = true;
+            this.trackWidth.Size = new System.Drawing.Size(45, 83);
+            this.trackWidth.TabIndex = 2;
+            this.trackWidth.TickFrequency = 2;
+            this.trackWidth.Scroll += new System.EventHandler(this.trackWidth_Scroll);
+            // 
+            // chc3D
+            // 
+            this.chc3D.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.chc3D.AutoSize = true;
+            this.chc3D.Location = new System.Drawing.Point(105, 37);
+            this.chc3D.Name = "chc3D";
+            this.chc3D.Size = new System.Drawing.Size(40, 28);
+            this.chc3D.TabIndex = 2;
+            this.chc3D.Text = "3D";
+            this.chc3D.UseVisualStyleBackColor = true;
+            this.chc3D.CheckedChanged += new System.EventHandler(this.chc3D_CheckedChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnStart.Location = new System.Drawing.Point(87, 310);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnStop.Location = new System.Drawing.Point(87, 344);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 26);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timerChart
+            // 
+            this.timerChart.Interval = 1000;
+            this.timerChart.Tick += new System.EventHandler(this.timerChart_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDepth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackWidth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.CheckBox chcMarkers;
+        private System.Windows.Forms.CheckBox chcFast;
+        private System.Windows.Forms.CheckBox chcColumn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackRotation;
+        private System.Windows.Forms.TrackBar trackDepth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackWidth;
+        private System.Windows.Forms.CheckBox chc3D;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timerChart;
+    }
+}
+
---
+++ chart/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace chart
+{
+    public partial class Form1 : Form
+    {
+        List<double> xVals = new List<double>();
+        List<double> yVals = new List<double>();
+
+        public Random rnd = new Random();
+        double tick = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+            LineGraph(0,10,0,0,15,5,3,0);
+            configStripline();
+        }
+
+        public void LineGraph(int xMin,int xMax,int xInterval,int yMin,int yMax,int yInterval,int borderWidth,int minorYgrid)
+        {
+            chart.ChartAreas[0].AxisX.Minimum=xMin;
+            chart.ChartAreas[0].AxisY.Minimum=yMin;
+            chart.ChartAreas[0].AxisX.Maximum=xMax;
+            chart.ChartAreas[0].AxisY.Maximum=yMax;
+            
+            chart.ChartAreas[0].AxisY.Interval = yInterval;
+            chart.ChartAreas[0].AxisY.MinorGrid.Enabled = true;
+            chart.ChartAreas[0].AxisY.MinorGrid.LineDashStyle = ChartDashStyle.DashDotDot;
+            chart.ChartAreas[0].AxisY.MinorGrid.LineColor = Color.LightBlue;
+            chart.ChartAreas[0].AxisY.MinorGrid.Interval = minorYgrid;
+            chart.Series[0].BorderWidth = borderWidth;
+            chart.Legends.Clear();
+            chart.Series[0].ChartType = SeriesChartType.Line;
+        }
+
+        private void chcColumn_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chcColumn.Checked)
+                chart.Series[0].ChartType = SeriesChartType.Column;
+            else
+                chart.Series[0].ChartType = SeriesChartType.Line;
+        }
+
+        private void chc3D_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chc3D.Checked)
+                chart.ChartAreas[0].Area3DStyle.Enable3D = true;
+            else
+                chart.ChartAreas[0].Area3DStyle.Enable3D = false;
+        }
+
+        private void chcFast_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chcFast.Checked)
+                timerChart.Interval = 250;
+            else
+                timerChart.Interval = 1000;
+        }
+
+        private void timerChart_Tick(object sender, EventArgs e)
+        {
+            xVals.Add(tick);
+            yVals.Add(10.0 * rnd.NextDouble());
+
+            if (tick > 20.0)
+            {
+                yVals.RemoveAt(0);
+                xVals.RemoveAt(0);
+            }
+            chart.ChartAreas[0].AxisX.Minimum = xVals[0];
+            chart.ChartAreas[0].AxisX.Maximum = tick;
+
+            chart.Series[0].Points.DataBindXY(xVals, yVals);
+            tick = tick + 0.5;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timerChart.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timerChart.Stop();
+        }
+
+        private void trackRotation_Scroll(object sender, EventArgs e)
+        {
+            //chart.ChartAreas[0].Area3DStyle.Inclination = trackRotation.Value;
+            chart.ChartAreas[0].Area3DStyle.Rotation = trackRotation.Value*10;
+        }
+
+        private void chcMarkers_CheckedChanged(object sender, EventArgs e)
+        {
+            chart.Series[0].MarkerSize = 10;
+            chart.Series[0].MarkerColor = Color.Tomato;
+            if (chcMarkers.Checked)
+                chart.Series[0].MarkerStyle = MarkerStyle.Square;
+            else
+                chart.Series[0].MarkerStyle = MarkerStyle.None;
+        }
+
+        private void trackWidth_Scroll(object sender, EventArgs e)
+        {
+            chart.Series[0].BorderWidth = trackWidth.Value;
+        }
+
+        private void trackDepth_Scroll(object sender, EventArgs e)
+        {
+            chart.ChartAreas[0].Area3DStyle.PointDepth = trackDepth.Value*10;
+        }
+
+        public void configStripline()
+        {
+            StripLine stripLine = new StripLine();
+            stripLine.StripWidth = 7;
+            stripLine.Interval = 0;
+            stripLine.IntervalOffset = 3;
+            stripLine.BackColor = Color.FromArgb(120, Color.Red);
+
+            chart.ChartAreas[0].AxisY.StripLines.Add(stripLine);
+        }
+    }
+}
